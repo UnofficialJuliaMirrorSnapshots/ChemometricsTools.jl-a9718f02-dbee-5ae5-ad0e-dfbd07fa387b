486c65af5e0406e3489c60a5559b0ee16815b3a2
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-11 10:15:00 +0000

    Snapshot of branch master taken on 2019-09-11T06:14:58.752-04:00 from "https://github.com/UnofficialJuliaMirror/ChemometricsTools.jl-a9718f02-dbee-5ae5-ad0e-dfbd07fa387b"

--- src/PlottingTools.jl
+++ src/PlottingTools.jl
@@ -5,10 +5,25 @@ end
 """
     QQ( Y1, Y2; Quantiles = collect( 1 : 99 ) ./ 100 )
 
-Returns a Plot object of a Quantile-Quantile plot between vectors `Y1` and `Y2` at the desired `Quantiles`.
+Returns a plotable object of a Quantile-Quantile plot between vectors `Y1` and `Y2` at the desired `Quantiles`.
 """
 function QQ( Y1, Y2; Quantiles = collect( 1 : 99 ) ./ 100 )
     return QQ( (Statistics.quantile!(Y1, Quantiles), Statistics.quantile!(Y2, Quantiles) ))
+end
+
+@recipe function f(qq::QQ)
+    m = sum( diff( qq.StoredTuple[ 1 ] ) ) / sum( diff( qq.StoredTuple[ 2 ] ) )
+    int = qq.StoredTuple[2][2] - (m * qq.StoredTuple[1][2])
+
+    seriestype := :scatter
+    title := "Quantile-Quantile Plot"
+    xlabel := Symbol("Quantile 1")
+    ylabel := Symbol("Quantile 2")
+    @series y := qq.StoredTuple
+
+    seriestype := :straightline
+    label := "Trend-Line"
+    @series y := ([0, 1], [int, int + m])
 end
 
 struct BlandAltman
@@ -23,7 +38,7 @@ end
 """
     BlandAltman(Y1, Y2; Confidence = 1.96)
 
-Returns a Plot object of a Bland-Altman plot between vectors `Y1` and `Y2` with a confidence limit of `Confidence`.
+Returns a Plottable object of a Bland-Altman plot between vectors `Y1` and `Y2` with a confidence limit of `Confidence`.
 """
 function BlandAltman(Y1, Y2; Confidence = 1.96)
     means = (Y1 .+ Y2) ./ 2.0
@@ -40,71 +55,133 @@ function BlandAltman(Y1, Y2; Confidence = 1.96)
     return BlandAltman( means, diffs, UpperLimit, Center, LowerLimit, Outliers )
 end
 
-
-"""
-    plotchem(QQ::{QQ, BlandAltman}; title )
-
-returns either a QQ Plot or a Bland-Altman plot with the defined `title`
-"""
-function plotchem(QQ::QQ; title = "Quantile-Quantile Plot" )
-    a = scatter( QQ.StoredTuple, title = title, xlabel = "Quantile 1", ylabel = "Quantile 2",
-                    label = "QQ")
-    diff(arr) = [ arr[d] - arr[d+1] for d in collect(1 : ( length( arr ) - 1 ))  ]
-    m = sum( diff( QQ.StoredTuple[ 1 ] ) ) / sum( diff( QQ.StoredTuple[ 2 ] ) )
-    int = QQ.StoredTuple[2][2] - (m * QQ.StoredTuple[1][2])
-    Plots.abline!(a, m, int, label = "Trend" )
-end
-
-function plotchem(BA::BlandAltman; title = "Bland Altman")
-    a = scatter( ( BA.means, BA.differences ), title = title, xlabel = "Means",
-                    ylabel = "Differences", label = "B.A.")
-    Plots.abline!(a, 0, BA.UpperLimit, color = :red )
-    Plots.abline!(a, 0, BA.Center, color = :red )
-    Plots.abline!(a, 0, BA.LowerLimit, color = :red )
+@recipe function f(BA::BlandAltman)
+    seriestype := :scatter
+    title := "Bland Altman"
+    xlabel := Symbol("Means")
+    ylabel := Symbol("Differences")
+    @series y := ( BA.means, BA.differences )
+    seriestype := :straightline
+    color := :red
+    @series y := ([0, 1], [BA.UpperLimit, BA.UpperLimit])
+    seriestype := :straightline
+    color := :red
+    @series y := ([0, 1], [BA.Center, BA.Center])
+    seriestype := :straightline
+    color := :red
+    @series y := ([0, 1], [BA.LowerLimit, BA.LowerLimit])
 end
 
 rectangle(w, h, x, y) = Shape(x .+ [0,w,w,0], y .+ [0,0,h,h])
-
 """
     IntervalOverlay(Spectra, Intervals, Err)
 
-Displays the relative error(`Err`) of each `interval` ontop of a `Spectra`.
+Creates a Plottable object to display the relative error(`Err`) of each `interval` ontop of a `Spectra`.
 """
-function IntervalOverlay(Spectra, Intervals, Err)
-    RelativeErr = Err ./ sum(Err);
-    a = plot(Spectra', xlabel = "bins", ylabel = "Absorbance / Relative CV Error", ylim = (0,7), legend = false);
-    for (i, line) in enumerate( Intervals )
-        scalethis = reduce(max, Spectra)  / reduce(max, RelativeErr)
+struct IntervalOverlay
+    Spectra
+    Intervals
+    Err
+end
+
+@recipe function f(IO::IntervalOverlay)
+    RelativeErr = IO.Err ./ sum(IO.Err);
+
+    seriestype := :path
+    title := "Spectral Error Overlay"
+    xlabel := Symbol("Bins")
+    ylabel := Symbol("Absorbance / Relative CV Error")
+    legend := false
+    @series y := IO.Spectra'
+
+    seriestype := :shape
+    opacity := 0.5
+    for (i, line) in enumerate( IO.Intervals )
+        scalethis = reduce(max, IO.Spectra)  / reduce(max, RelativeErr)
         w = line[end] - line[1]
         h = RelativeErr[i] * scalethis
         xloc = line[1]
-        plot!(rectangle(w,h,xloc,0), opacity=.5)
+        @series y := rectangle(w,h,xloc,0)
     end
-    return a
 end
+
+"""
+    PCA/LDA(::Union{PCA, LDA}; Axis = [1,2])
+
+Plots scores of PCA/LDA analysis.
+"""
+@recipe function f(DA::Union{PCA, LDA}; Axis = [1,2])
+    @assert(length(Axis) == 2, "Axis for PCA/LDA plot must be of length 2." )
+    exvar = round.(ExplainedVariance(DA) .* 10000) /100
+    seriestype := :scatter
+    if isa(DA, PCA)
+        title := "PCA Scores Plot"
+        xlabel := "PCA $(Axis[1]) [$(exvar[Axis[1]]) %]"
+        ylabel := "PCA $(Axis[2]) [$(exvar[Axis[2]]) %]"
+    else
+        title := "LDA Scores Plot"
+        xlabel := "DA $(Axis[1]) [$(exvar[Axis[1]]) %]"
+        ylabel := "DA $(Axis[2]) [$(exvar[Axis[2]]) %]"
+    end
+    @series y := ( DA.Scores[:,Axis[2]], DA.Scores[:,Axis[1]] )
+end
+
+# x = -pi:(pi/200):pi;
+# A = transpose((sin.(x) .+ 1.0) .+ randn(400,3) ./ 50);
+# Intervals = [ (((i-1) * 10) + 1, ((i) * 10)) for i in 1:40 ];
+# Err = rand(40);
+# IOo = IntervalOverlay(A,Intervals,Err)
+# Plots.plot(IOo)
 
 """
     DiscriminantAnalysisPlot(DA, GD, YHot, LblEncoding, UnlabeledData, Axis = [1,2], Confidence = 0.90)
 ...
 """
-function DiscriminantAnalysisPlot(  DA, GD, YHot, LblEncoding, UnlabeledData,
-                                    Axis = [1,2], Confidence = 0.90)
-    @assert all(Axis .<= size(Data)[2])
+struct DAPlot
+    DA
+    GD
+    YHot
+    LblEncoding
+    UnlabeledData
+    Axis
+    Confidence
+end
+
+DiscriminantAnalysisPlot(DA, GD, YHot, LblEncoding, UnlabeledData;
+                    Axis = [1,2], Confidence = 0.90) = DAPlot( DA, GD, YHot,
+                    LblEncoding, UnlabeledData, Axis, Confidence)
+
+@recipe function f(dap::DAPlot)
+    exvar = round.(ExplainedVariance(dap.DA) .* 10000) /100
+    @assert all(dap.Axis .<= size(dap.UnlabeledData)[2])
     A = []
-    for c in 1:size(filterYHOT)[2]
-        whichrows = filterYHOT[:,c] .== 1.0
-        if c == 1
-            A = scatter(DA.scores[whichrows,Axis[1]], DA.scores[whichrows,Axis[2]],  label = LblEncoding.ToCold[c])
-        else
-            scatter!(A, DA.scores[whichrows,Axis[1]], DA.scores[whichrows,Axis[2]],  label = LblEncoding.ToCold[c])
-        end
-        elipse = ConfidenceEllipse(GD.ProjectedClassCovariances[c], GD.ProjectedClassMeans[c,:],
-                    Confidence, Axis; pointestimate = 180 );
-        plot!(elipse[:,1], elipse[:,2], label = "", color = :black, linestyle = :dash, linewidth = 2);
+    Classes = size(dap.YHot)[2]
+    colors = get_color_palette(:auto, plot_color(:white), Classes)
+    for c in 1:Classes
+        #Labelled data
+        whichrows = dap.YHot[:,c] .== 1.0
+        seriestype := :scatter
+        color := colors[c]
+        label := dap.LblEncoding.ToCold[c]
+        @series y := ( dap.DA.Scores[whichrows,dap.Axis[1]], dap.DA.Scores[whichrows,dap.Axis[2]] )
+        #Make confidence ellipse
+        elipse = ConfidenceEllipse(dap.GD.ProjectedClassCovariances[c], dap.GD.ProjectedClassMeans[c,:],
+                    dap.Confidence, dap.Axis; pointestimate = 180 );
+
+        seriestype := :path
+        color := :black
+        linestyle := :dash
+        linewidth := 2
+        label := ""
+        @series y := (elipse[:,1], elipse[:,2])
     end
-    exvar = round.(ExplainedVariance(DA) .* 1000) /10
-    scatter!(A, UnlabeledData[:,Axis[1]], UnlabeledData[:,Axis[2]], color = :black, label = "", markershape = :hexagon,
-            xaxis = "DA $(Ax[1]) [$(exvar[Ax[1]]) %]", yaxis = "DA $(Ax[2]) [$(exvar[Ax[2]]) %]",
-            legend = :topleft);
-    return A
+    #New Data
+    seriestype := :scatter
+    color := :black
+    markershape = :star
+    label := ""
+    legend := :topleft
+    xlabel := "DA $(dap.Axis[1]) [$(exvar[dap.Axis[1]]) %]"
+    ylabel := "DA $(dap.Axis[2]) [$(exvar[dap.Axis[2]]) %]"
+    @series y := ( dap.UnlabeledData[:,dap.Axis[1]] , dap.UnlabeledData[:,dap.Axis[2]] )
 end
